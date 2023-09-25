01c6e8b74bfb0875582068f3e603c4ff2ee921a4
Gussstavof <g717397@gmail.com>
2023-09-25 18:30:24 -0300

    VideoService refactored

---
+++ src/main/java/com/challenge/alura/AluraFlix/core/entities/videos/VideoResponse.java
@@ -0,0 +1,29 @@
+package com.challenge.alura.AluraFlix.core.entities.videos;
+
+import com.challenge.alura.AluraFlix.core.entities.categories.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class VideoResponse {
+    private  String id;
+    private String title;
+    private String description;
+    private Category category;
+    private String url;
+
+    public VideoResponse(Video video) {
+        this.id = video.getId();
+        this.title = video.getTitle();
+        this.description = video.getDescription();
+        this.category = video.getCategory();
+        this.url = video.getUrl();
+    }
+}
